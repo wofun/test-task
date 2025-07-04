c924996884b63779bd8298ec2fe948e772d817fa
Volodymyr Benko <volod.benko@gmail.com>
2025-07-04 11:55:36 +0300

    Initialized basic Yii2. Created .env file and installed appropriate plugin to read it. Installed dektrium/yii2-user and dektrium/yii2-rbac modules

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=' . env('DB_HOST') . ':' . env('DB_PORT') . ';dbname=' . env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Home page';
+?>
+<div class="site-index">
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="mb-5">Home page</h1>
+        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis quas eligendi consectetur eveniet, inventore accusamus quod magni animi veniam nemo. Sequi possimus similique vero dolor voluptas nulla quod, unde cumque.</p>
+
+        <p class="mt-5"><a class="btn btn-lg btn-success" href="/admin">Visit Admin Panel</a></p>
+    </div>
+</div>
